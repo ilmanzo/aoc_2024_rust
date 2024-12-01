d3b1861235642aaddd4c25a89b5a66e013b3a672
Andrea Manzini <ilmanzo@gmail.com>
2024-12-01 07:47:24 +0100

    day1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,9 +1,9 @@
+use adv_code_2024::*;
 use anyhow::*;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
-use adv_code_2024::*;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 const DAY: &str = "01";
 const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
@@ -17,37 +17,40 @@ const TEST: &str = "\
 3   3
 ";
 
-fn  parse_lists<R: BufRead>(reader:R) -> (Vec<isize>, Vec<isize>) {
-    let lines = reader.lines().map(|l|l.unwrap());
+fn parse_lists<R: BufRead>(reader: R) -> (Vec<isize>, Vec<isize>) {
+    let lines = reader.lines().map(|l| l.unwrap());
     let mut left = Vec::new();
     let mut right = Vec::new();
     for line in lines {
         if line.is_empty() {
             continue;
         }
-        let nums = line.split_whitespace().map(|num| num.parse::<isize>().unwrap()).collect::<Vec<isize>>();
+        let nums = line
+            .split_whitespace()
+            .map(|num| num.parse::<isize>().unwrap())
+            .collect::<Vec<isize>>();
         left.push(nums[0]);
         right.push(nums[1]);
     }
     left.sort();
     right.sort();
-    (left,right)
+    (left, right)
 }
 
 fn part1(left: &[isize], right: &[isize]) -> isize {
-    left.iter().zip(right).fold(0, |acc, (l, r)| acc + (l - r).abs())
+    left.iter()
+        .zip(right)
+        .fold(0, |acc, (l, r)| acc + (l - r).abs())
 }
 
 fn part2(left: &[isize], right: &[isize]) -> isize {
     let mut sum = 0;
     for l in left {
-        let count=right.iter().filter(|r| **r == *l).count();
-        sum += l*count as isize;
+        let count = right.iter().filter(|r| **r == *l).count();
+        sum += l * count as isize;
     }
     sum
 }
-
-
 
 fn main() -> Result<()> {
     start_day(DAY);
@@ -57,14 +60,14 @@ fn main() -> Result<()> {
     let input_file = BufReader::new(File::open(INPUT_FILE)?);
     let (mut left, mut right) = parse_lists(input_file);
     //assert_eq!(11, part1(&left,&right));
-    let result = time_snippet!(part1(&left,&right));
+    let result = time_snippet!(part1(&left, &right));
     println!("Result = {}", result);
     //endregion
 
     //region Part 2
     println!("\n=== Part 2 ===");
     //assert_eq!(31, part2(&left,&right));
-    let result = time_snippet!(part2(&left,&right));
+    let result = time_snippet!(part2(&left, &right));
     println!("Result = {}", result);
     //endregion
 
